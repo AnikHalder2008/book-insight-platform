7a3eff2092983b9d9138f79e9be1d0fbc5ed338c
AnikHalder2008 <anikhalder404@gmail.com>
2026-04-17 13:19:02 +0530

    Update urls.py

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('books/', views.get_books, name='get_books'),
+    path('books/<int:pk>/', views.get_book, name='get_book'),
+    path('add-book/', views.add_book, name='add_book'),
+    path('query/', views.ask_question, name='ask_question'),
+]
