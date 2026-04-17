46f998a7cfbfbe2cd9aa2cac55a2dba7b86698a5
AnikHalder2008 <anikhalder404@gmail.com>
2026-04-17 14:39:08 +0530

    Update Loader.js

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,18 +1,11 @@
 import React from "react";
 
-function BookCard({ book }) {
+function Loader() {
   return (
-    <div className="book-card">
-      <h3>{book.title}</h3>
-      <p><strong>Author:</strong> {book.author}</p>
-      <p><strong>Rating:</strong> {book.rating || "N/A"}</p>
-      <p><strong>Description:</strong> {book.description || "No description available"}</p>
-
-      <a href={book.url} target="_blank" rel="noreferrer">
-        View Book
-      </a>
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <p>Loading...</p>
     </div>
   );
 }
 
-export default BookCard;
+export default Loader;
