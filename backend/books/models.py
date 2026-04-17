952e658a2ab1bb7368598756d5d34881422c6860
AnikHalder2008 <anikhalder404@gmail.com>
2026-04-17 13:14:38 +0530

    Update models.py

--- backend/books/models.py
+++ backend/books/models.py
@@ -1,1 +1,11 @@
+from django.db import models
 
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    rating = models.FloatField(null=True, blank=True)
+    description = models.TextField()
+    url = models.URLField()
+
+    def __str__(self):
+        return self.title
