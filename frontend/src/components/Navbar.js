e58b300d7c0dd9102e6b1037e32f60a2ad4fc404
AnikHalder2008 <anikhalder404@gmail.com>
2026-04-17 14:40:25 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-function Loader() {
+function Navbar() {
   return (
-    <div style={{ textAlign: "center", marginTop: "20px" }}>
-      <p>Loading...</p>
-    </div>
+    <nav style={styles.navbar}>
+      <h2 style={styles.logo}>📚 Book Insight</h2>
+    </nav>
   );
 }
 
-export default Loader;
+const styles = {
+  navbar: {
+    backgroundColor: "#007bff",
+    padding: "15px",
+    color: "white",
+    textAlign: "center",
+  },
+  logo: {
+    margin: 0,
+  },
+};
+
+export default Navbar;
