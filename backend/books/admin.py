ad4cd46f53fb0bf00fa68f8988af5f3e9b9d29ac
AnikHalder2008 <anikhalder404@gmail.com>
2026-04-17 13:16:21 +0530

    Update admin.py

--- backend/books/admin.py
+++ backend/books/admin.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
+from .models import Book
 
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'rating')
+    search_fields = ('title', 'author')
+    list_filter = ('rating',)
+
+
+admin.site.register(Book, BookAdmin)
